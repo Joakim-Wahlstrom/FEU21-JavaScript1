be114c9ae4699a488149b148b744808965e13558
Joakim <wahlstrom.joakim@gmail.com>
2021-12-06 10:00:31 +0100

    lektion 1

---
+++ Lektion-1/2-console/script.js
@@ -0,0 +1,31 @@
+// en kommentar på en rad  (ctrl + *)
+/*
+  en kommentar kan skrivas såhär
+  om det är flera rader     (shift + alt + a)
+*/
+
+
+// Skriver ut någonting i konsolen
+console.log('Här skriver jag ut någonting i konsolen');
+console.warn('Detta är en varning')
+console.error('Detta är ett felmeddelande');
+
+
+// console.lg('hej');
+
+
+// Skriver ut på själva hemsidan
+document.body.innerText = 'Detta är en text';
+
+
+// Högnivå språk  (mellan användaren och systemet)
+// ----------------------------------------------
+// (Javascript)
+// Python
+// C# / Java
+
+// Lågnivå språk (mellan systemet och hårdvara)
+// ----------------------------------------------
+// C++
+// C
+// Mikrokod
