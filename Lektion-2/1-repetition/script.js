9aab5cf5e9f3a3d37e5e133c34bc43575ea43cd5
Joakim <wahlstrom.joakim@gmail.com>
2021-12-08 09:38:46 +0100

    Lektion 2

---
+++ Lektion-2/1-repetition/script.js
@@ -0,0 +1,41 @@
+// CONSOLE
+
+// Felsökning om någonting går fel
+// let app;
+// let app;
+
+// console.log('vanligt meddelande');
+// console.warn('varnings meddelande');
+// console.error('felmeddelande');
+
+// DOM
+// document.body.innerText = '<h1>Hejsan</h1>';
+// document.body.innerHTML = '<h1>Hejsan</h1>';
+// document.body.createElement('div').innerText = 'hej'; // ger ett felmeddelande i consolen.
+// document.body.appendChild(document.createElement('div'));
+
+// const results = document.querySelector('#results');
+// const input = document.querySelector('.input-class');
+// const input = document.getElementsByName('input-name')[0];
+// const input = document.getElementById('input-id');
+
+// let res = document.querySelector('#input-id').id;
+
+
+// res = input.value;
+// results.innerText = res;
+
+// console.log(input);
+// console.log(res);
+
+
+// VARIABLES
+
+// name = 'Joakim';           // Deklarerar någonting globalt. GÖR INTE SÅHÄR
+// var name = 'Joakim';       // Deklarerar någonting globalt. UNDVIK DET HÄR
+// let name = 'Joakim';       // Deklarerar någonting lokalt i ett scope. GÖR DET HÄR
+// const name = 'Joakim';     // Deklarerar någonting skrivskyddat lokalt. GÖR DET HÄR
+
+// name = 'Hans';
+
+// console.log(name);
