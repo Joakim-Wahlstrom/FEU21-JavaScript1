e84a5600aa2676b0d6bc938a74e4b01e97dd6cf3
Joakim <wahlstrom.joakim@gmail.com>
2021-12-15 13:53:02 +0100

    lektion 4

---
+++ Lektion-4/3-DOM/2-events/script.js
@@ -0,0 +1,84 @@
+const btn = document.querySelector('#btn-alert');
+const square = document.querySelector('#square');
+const loginForm = document.querySelector('#loginForm');
+const firstName = document.querySelector('#firstName');
+
+// function alert1() {
+//   alert('Warning')
+// }
+
+// btn.addEventListener('click', alert1)
+// btn.removeEventListener('click', alert1)
+
+
+// btn.addEventListener('click', () => {
+//   console.log('klickade på knappen')
+// })
+// btn.addEventListener('dblclick', () => {
+//   console.log('klickade på knappen')
+// })
+// btn.addEventListener('mousedown', () => {
+//   console.log('eventet kördes')
+// })
+// btn.addEventListener('mouseup', () => {
+//   console.log('eventet kördes')
+// })
+// btn.addEventListener('mouseenter', () => {
+//   console.log('eventet kördes')
+// })
+// btn.addEventListener('mouseleave', () => {
+//   console.log('eventet kördes')
+// })
+btn.addEventListener('mouseover', (event) => {
+  console.log('eventet kördes')
+})
+square.addEventListener('mousemove', event => {
+  // console.log(event.offsetX, event.offsetY)
+  square.style.background = `rgb(${event.offsetX}, ${event.offsetY}, 0)`
+})
+
+
+
+// firstName.addEventListener('keyup', (e) => {
+//   console.log(e.key)
+// })
+// firstName.addEventListener('keydown', (e) => {
+//   console.log(e.key)
+// })
+// firstName.addEventListener('focus', (e) => {
+//   console.log(this)
+//   firstName.classList.add('focus-red')
+// })
+// firstName.addEventListener('blur', (e) => {
+//   firstName.classList.remove('focus-red')
+// })
+
+// firstName.addEventListener('blur', function(e){
+//   console.log(this)
+//   this.classList.remove('focus-red')
+// })
+
+const test = function() {
+  console.log(this)
+}
+const test2 = () => {
+  console.log(this)
+}
+
+firstName.addEventListener('focus', test)
+
+btn.addEventListener('click', test)
+
+
+
+
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if(firstName.value.trim() === '')
+    return;
+
+    console.log(firstName.value)
+
+    console.log('validerar formulär')
+    console.log('validering klar! skickar till databasen')
+})
