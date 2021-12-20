304a3186aaf05b25298c310a188cf43aa9065bf3
Joakim <wahlstrom.joakim@gmail.com>
2021-12-20 09:45:08 +0100

    lektion 5

---
+++ Lektion-5/1-repetition(öu3)/script.js
@@ -0,0 +1,90 @@
+const printName = (name) => {
+  console.log('Hello ' + name);
+}
+// printName('Joakim');
+
+for(let i = 0; i < 5; i++) {
+  // console.log(i)
+  // printName('Joakim')
+}
+
+
+// const numberToColor = (nr) => {
+//   let answer;
+//   if(nr < 0){
+//     // return 'black'
+//     answer = 'black';
+//   } else {
+
+//     switch(true) {
+//       case nr < 13:
+//         // return 'white'
+//         answer = 'white'
+//         break;
+//       case nr < 19:
+//         // return 'green'
+//         answer = 'green'
+//         break;
+//       case nr < 26:
+//         // return 'red'
+//         answer = 'red'
+//         break;
+//       case nr < 100:
+//         // return 'blue'
+//         answer = 'blue'
+//         break;
+//       default:
+//         // return 'black'
+//         answer = 'black'
+//         break;
+//     }
+//   }
+//   console.log(answer)
+//   // return answer;
+// }
+
+const numberToColor = (nr) => {
+  if(nr >= 0 && nr <= 12) {
+    console.log('white')
+  }
+  else if(nr >= 13 && nr <= 18) {
+    console.log('green')
+  }
+  else if(nr >= 19 && nr <= 25) {
+    console.log('red')
+  }
+  else if(nr >= 26 && nr <= 99) {
+    console.log('blue')
+  }
+  else {
+    console.log('black')
+  }
+}
+
+// numberToColor(12);
+// numberToColor(18);
+// numberToColor(25);
+// numberToColor(99);
+// numberToColor(89234);
+// numberToColor(-12);
+
+
+const namesArray = ['Joakim', 'Hans', 'Tommy', 'Jeanette'];
+// const namesDIV = document.querySelector('#names');
+
+// namesArray.forEach(name => {
+//   namesDIV.innerHTML += `<p>${name}</p>`
+// })
+
+
+
+let namesDIV = document.createElement('div');
+namesDIV.id = 'names';
+
+document.body.appendChild(namesDIV);
+
+namesArray.forEach(name => {
+  let p = document.createElement('p');
+  p.innerText = name;
+  namesDIV.appendChild(p);
+})
