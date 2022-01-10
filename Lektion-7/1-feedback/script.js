ffe8eeeffb66bf81724ff8879f08f337e0ab8160
Joakim <wahlstrom.joakim@gmail.com>
2022-01-10 13:54:11 +0100

    lektion 7

--- Lektion-7/1-feedback/script.js
+++ Lektion-7/1-feedback/script.js
@@ -2,17 +2,23 @@ const form = document.querySelector('#regForm');
 const firstName = document.querySelector('#firstName');
 const lastName = document.querySelector('#lastName');
 const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const password2 = document.querySelector('#repeat-password');
+const tac = document.querySelector('#tac');
 
 
 const validateText = (input) => {
   if(input.value.trim() === '') { 
     setError(input, 'Name can\'t be empty')
+    return false;
   }
   else if(input.value.trim().length < 2) {
     setError(input, 'Name must be atleast 2 chars long')
+    return false;
   }
   else {
     setSuccess(input)
+    return true;
   }
 }
 
@@ -21,12 +27,60 @@ const validateEmail = email => {
 
   if(email.value.trim() === '') {
     setError(email, 'You need to enter a email address');
+    return false;
   } 
   else if(!regEx.test(email.value)) {
     setError(email, 'Email address is not valid');
+    return false;
   }
   else {
     setSuccess(email)
+    return true;
+  }
+}
+
+const validatePassword = input => {
+  if(input.value.trim() === '') { 
+    setError(input, 'Password can\'t be empty')
+    return false;
+  }
+  else if(input.value.trim().length < 5) {
+    setError(input, 'Password must be atleast 5 chars long')
+    return false;
+  }
+  else if (!/\d/.test(input.value)) {
+    setError(input, 'Password must contain atleast one number')
+    return false;
+  }
+  else {
+    setSuccess(input)
+    return true;
+  }
+}
+
+const samePassword = (password, repeatPassword) => {
+  if(repeatPassword.value.trim() === '') {
+    setError(repeatPassword, 'Password can\'t be empty');
+    return false;
+  }
+  else if(password.value.trim() !== repeatPassword.value.trim()) {
+    setError(repeatPassword, 'Passwords must match');
+    return false;
+  }
+  else {
+    setSuccess(repeatPassword);
+    return true;
+  }
+}
+
+const validateCheck = checkbox => {
+  if(!checkbox.checked) {
+    setError(checkbox, 'You must accept the terms')
+    return false;
+  }
+  else {
+    setSuccess(checkbox)
+    return true;
   }
 }
 
@@ -45,13 +99,67 @@ const setSuccess = input => {
 
 
 
+
+const validate = input => {
+  switch(input.type) {
+    case 'text': return validateText(input)
+    case 'email': return validateEmail(input)
+    case 'password': return input.id !== 'repeat-password' ? validatePassword(input) : samePassword(password, password2)
+    case 'checkbox': return validateCheck(input)
+    default:
+      break;
+  }
+}
+
+
 form.addEventListener('submit', e => {
   e.preventDefault();
 
-  validateText(firstName);
-  validateText(lastName);
-  validateEmail(email);
 
-  // Skapa en användare om valideringen går igenom
+
+  // validateText(firstName)
+  // validateText(lastName)
+  // validateEmail(email)
+  // validatePassword(password)
+  // samePassword(password, password2)
+  // validateCheck(tac)
+
+  // if (validateText(firstName) &&
+  // validateText(lastName) &&
+  // validateEmail(email) &&
+  // validatePassword(password) &&
+  // samePassword(password, password2) &&
+  // validateCheck(tac)) {
+
+  //   const user = {
+  //     id: Date.now().toString(),
+  //     firstName : firstName.value,
+  //     lastName : lastName.value,
+  //     email : email.value,
+  //     password : password.value
+  //   }
+  //   console.log(user);
+  // }
+  // else {
+  //   console.log('errororrrr')
+  // }
+
+  errors = []
+
+  for(let i = 0; i < form.length; i++) {
+    errors[i] = validate(form[i])
+  }
+  console.log(errors)
+
+  if(!errors.includes(false)) {
+    const user = {
+      id: Date.now().toString(),
+      firstName : firstName.value,
+      lastName : lastName.value,
+      email : email.value,
+      password : password.value
+    }
+    console.log(user);
+  }
 
 })
